237ca4e05c8d5a543cd634069f497c66b3d850b1
BILON Ikram <bilonikram20@gmail.com>
2025-06-26 13:17:07 +0100

    Récupérer tous les scripts d'une société (avec gig peuplé)

--- src/routes/scriptRoutes.js
+++ src/routes/scriptRoutes.js
@@ -5,5 +5,7 @@ const scriptController = require('../controllers/scriptController');
 
 // Récupérer tous les scripts d'un gig
 router.get('/gig/:gigId', scriptController.getScriptsForGig);
+// Récupérer tous les scripts d'une société (avec gig peuplé)
+router.get('/company/:companyId', scriptController.getScriptsForCompany);
 
 module.exports = router; 
