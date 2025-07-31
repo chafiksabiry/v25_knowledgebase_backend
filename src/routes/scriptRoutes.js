c29d6dad267b0c709b6f25368d011a57c5f6b3dc
BILON Ikram <bilonikram20@gmail.com>
2025-07-31 12:13:51 +0100

    activate / desactivate scripts

--- src/routes/scriptRoutes.js
+++ src/routes/scriptRoutes.js
@@ -7,6 +7,8 @@ const scriptController = require('../controllers/scriptController');
 router.get('/gig/:gigId', scriptController.getScriptsForGig);
 // Récupérer tous les scripts d'une société (avec gig peuplé)
 router.get('/company/:companyId', scriptController.getScriptsForCompany);
+// Mettre à jour le statut d'un script (activer/désactiver)
+router.put('/:scriptId/status', scriptController.updateScriptStatus);
 // Supprimer un script par son ID
 router.delete('/:scriptId', scriptController.deleteScript);
 
