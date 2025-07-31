19af498d69d647ea2c063774680852375e3a027d
BILON Ikram <bilonikram20@gmail.com>
2025-07-31 15:27:40 +0100

    1st version of regenerating script

--- src/routes/scriptRoutes.js
+++ src/routes/scriptRoutes.js
@@ -5,10 +5,22 @@ const scriptController = require('../controllers/scriptController');
 
 // Récupérer tous les scripts d'un gig
 router.get('/gig/:gigId', scriptController.getScriptsForGig);
+
 // Récupérer tous les scripts d'une société (avec gig peuplé)
 router.get('/company/:companyId', scriptController.getScriptsForCompany);
+
 // Mettre à jour le statut d'un script (activer/désactiver)
 router.put('/:scriptId/status', scriptController.updateScriptStatus);
+
+// Régénérer complètement un script
+router.post('/:scriptId/regenerate', scriptController.regenerateScript);
+
+// Affiner une partie spécifique du script avec un prompt
+router.post('/:scriptId/refine', scriptController.refineScriptPart);
+
+// Modifier directement le contenu d'un script
+router.put('/:scriptId/content', scriptController.updateScriptContent);
+
 // Supprimer un script par son ID
 router.delete('/:scriptId', scriptController.deleteScript);
 
